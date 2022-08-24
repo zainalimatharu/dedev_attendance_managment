0851ca9d6c49063326733107221dceb79443e2fb
Zain Ali <zainalimatharu@gmail.com>
2022-08-24 11:45:57 +0500

    added sonar setup (CI)

--- api/config/db.js
+++ api/config/db.js
@@ -1,19 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.mongoURI, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB Connected...');
+    await mongoose.connect(
+      "mongodb+srv://zainali:2015Ag55rehan3@cluster0.rlj2s.mongodb.net/gsas?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+      }
+    );
   } catch (err) {
     console.error(err.message);
     // Exit process with failure: "1" is failure code, while "0" is normal exit code
     process.exit(1);
   }
 };
+
+connectDB();
+
+mongoose.connection.on("connected", async () => {
+  const newsSchema = mongoose.Schema(
+    {
+      test: String,
+    },
+    { collection: "news" }
+  );
+
+  const News = mongoose.model("New", newsSchema);
+
+  const res = await News.insertMany({ test: "abc" });
+
+  console.log(res);
+});
 
 module.exports = { connectDB };
